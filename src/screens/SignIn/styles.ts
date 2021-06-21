118882b1f599d6900bbd5d472fc2fabe9e927e3c
bruno <bruno.araujo@uppertools.com.br>
2021-06-20 22:10:22 -0300

    gameplay: Tela de splash

---
+++ src/screens/SignIn/styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+export const style = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+    input: {
+        height: 50,
+        width: 200
+    }
+})
